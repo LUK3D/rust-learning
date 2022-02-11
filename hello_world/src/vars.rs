dac2ca3ec5298fef7f1bfa57b40c2d061687b691
Filipe Lukebana <filipelukebana@gmail.com>
2022-02-11 02:58:56 +0100

    Updates

--- hello_world/src/vars.rs
+++ hello_world/src/vars.rs
@@ -1,5 +1,19 @@
 pub fn run(){
     let name = "Filipe Lukebana";
+    let mut age = 24;
+    age +=1;
+    println!("my name is {} and I am  {}", name, age);
 
-    println!("my name is {}", name);
+
+    //Definindo constantes
+
+    const ID:i32 = 001;
+
+    println!("ID: {}",ID);
+
+    //Multiplas Variaveis
+
+    let (meu_nome, minha_idade) = ("Filipe",25);
+
+    println!("Nome: {meu_nome} Idade: {minha_idade}",meu_nome=meu_nome,minha_idade=minha_idade);
 }
