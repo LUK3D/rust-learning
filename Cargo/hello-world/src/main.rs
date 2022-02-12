a89fcc842f8e12b14ce283428671fa0939ea6082
Filipe Lukebana <filipelukebana@gmail.com>
2022-02-13 00:16:39 +0100

    Numbers...

--- Cargo/hello-world/src/main.rs
+++ Cargo/hello-world/src/main.rs
@@ -1,3 +1,10 @@
+/** # Teste basico */
 fn main() {
-    println!("Hello, Luk3D!");
+    let mut x:u64 = 45; // u64 tipo que não suporta valores negativos (diferente dp i64)
+    let mut f: f64 = 0.5;
+
+    print!("{}",x);
+    
+    x = 40;
+    print!("{}",x);
 }
